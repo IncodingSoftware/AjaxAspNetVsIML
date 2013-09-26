e69964de570074f4f8368bac806d6794ff517042
Incoding <vlad.kopachinsky@gmail.com>
2013-09-26 13:36:35 +0400

    init repository

---
+++ AjaxActionVsIml/Controllers/HomeController.cs
@@ -0,0 +1,76 @@
+﻿namespace AjaxActionVsIml.Controllers
+{
+    #region << Using >>
+
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web.Mvc;
+    using Incoding.MvcContrib;
+
+    #endregion
+
+    public class HomeController : Controller
+    {
+        #region Static Fields
+
+        public static Dictionary<string, KeyValueVm> data = GetData();
+
+        #endregion
+
+        #region Http action
+
+        [HttpPost]
+        public ActionResult Delete(string id)
+        {
+            data.Remove(id);
+            return IncodingResult.Success();
+        }
+
+        [HttpPost]
+        public ActionResult DeleteAsp(string id)
+        {
+            data.Remove(id);
+            return PartialView("Index_Asp_Tbody", data.Select(r => r.Value)
+                                                      .ToList());
+        }
+
+        [HttpGet]
+        public ActionResult Fetch()
+        {
+            return IncodingResult.Success(data.Select(r => r.Value)
+                                              .ToList());
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            data = GetData();
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Index_Asp()
+        {
+            return View(data.Select(r => r.Value)
+                            .ToList());
+        }
+
+        [HttpGet]
+        public ActionResult Index_Iml()
+        {
+            return View();
+        }
+
+        #endregion
+
+        static Dictionary<string, KeyValueVm> GetData()
+        {
+            return new Dictionary<string, KeyValueVm>
+                       {
+                               { "1", new KeyValueVm("1", "Vlad") }, 
+                               { "2", new KeyValueVm("2", "Vlad") }, 
+                               { "3", new KeyValueVm("3", "Vlad") }, 
+                       };
+        }
+    }
+}
